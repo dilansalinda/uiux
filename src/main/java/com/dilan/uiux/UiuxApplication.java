8a174dd69c348411e1febfcae6db224d41aabd75
dilan <dilan2012salinda@gmail.com.com>
2020-04-29 23:20:29 +0530

    add datas

--- src/main/java/com/dilan/uiux/UiuxApplication.java
+++ src/main/java/com/dilan/uiux/UiuxApplication.java
@@ -1,40 +1,35 @@
 package com.dilan.uiux;
 
-import com.dilan.uiux.Services.UserService;
+import com.dilan.uiux.Repositories.UserRepository;
 import com.dilan.uiux.entities.User;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootApplication
 @EnableJpaAuditing
 public class UiuxApplication {
 
+    @Autowired
+    private UserRepository userRepository;
+
     public static void main(String[] args) {
-
-        UserService userService = new UserService();
-        List<User> users = new ArrayList<>();
-        User user1 = new User();
-        user1.setId(1L);
-        user1.setEmail("dilansalinda@mail.com");
-        user1.setFull_name("Dilan Salinda");
-        user1.setUsername("dilansalinda");
-
-        User user2 = new User();
-        user2.setId(2L);
-        user2.setEmail("samankumara@mail.com");
-        user2.setFull_name("Saman Kumara");
-        user2.setUsername("samankumara");
-
-        users.add(user1);
-        users.add(user2);
-        for (User u : users) {
-            userService.saveUser(u);
-        }
         SpringApplication.run(UiuxApplication.class, args);
     }
 
+
+    @Bean
+    InitializingBean sendDatabase() {
+        return () -> {
+            userRepository.save(new User("d", "d", "d", "d"));
+            userRepository.save(new User("a", "a", "a", "a"));
+            userRepository.save(new User("f", "f", "f", "f"));
+            userRepository.save(new User("w", "w", "w", "w"));
+        };
+    }
 }
+
+
--- src/main/java/com/dilan/uiux/entities/User.java
+++ src/main/java/com/dilan/uiux/entities/User.java
@@ -20,6 +20,13 @@ public class User {
     private String password;
     private String full_name;
 
+    public User(String username, String email, String password, String full_name) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.full_name = full_name;
+    }
+
     public Long getId() {
         return id;
     }
